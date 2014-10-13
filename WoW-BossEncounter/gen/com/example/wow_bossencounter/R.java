750c22982b1af05eefd024a64bd62b9e450c4be1
paolinidiego <paolinidiego@gmail.com>
2014-10-14 00:12:52 +0200

    aggiunto bottone di prova

--- WoW-BossEncounter/gen/com/example/wow_bossencounter/R.java
+++ WoW-BossEncounter/gen/com/example/wow_bossencounter/R.java
@@ -1106,11 +1106,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05004d;
+        public static final int action_settings=0x7f05004e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int bot=0x7f05003f;
+        public static final int button1=0x7f050048;
         public static final int bwd=0x7f05003e;
         public static final int cata_guida=0x7f050045;
         public static final int catalogo=0x7f05003d;
@@ -1128,7 +1129,7 @@ containing a value of this type.
         public static final int firelands=0x7f050041;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
-        public static final int icc=0x7f05004c;
+        public static final int icc=0x7f05004d;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
@@ -1136,7 +1137,7 @@ containing a value of this type.
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int mop_guida=0x7f050046;
-        public static final int naxxramas=0x7f050049;
+        public static final int naxxramas=0x7f05004a;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
@@ -1164,15 +1165,15 @@ containing a value of this type.
         public static final int t4w=0x7f050040;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
-        public static final int toc=0x7f05004b;
+        public static final int toc=0x7f05004c;
         public static final int top_action_bar=0x7f050020;
-        public static final int ulduar=0x7f05004a;
+        public static final int ulduar=0x7f05004b;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
         public static final int wod_guida=0x7f050047;
         public static final int wrat_guida=0x7f050044;
-        public static final int wratlogo=0x7f050048;
+        public static final int wratlogo=0x7f050049;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
