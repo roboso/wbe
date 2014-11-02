7aa3334ec97bdedaa2c26e9bfa49b43d8c801a78
Nhova85 <valentino0185@gmail.com>
2014-11-02 14:48:07 +0100

    Cata completata.

---
+++ WoW-BossEncounter/src/com/example/wow_bossencounter/BastioneListaBoss.java
@@ -0,0 +1,32 @@
+package com.example.wow_bossencounter;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.app.Activity; 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+
+public class BastioneListaBoss extends Activity 
+implements OnClickListener {
+
+	
+		
+		@Override    
+         public void onClick(View View) {    
+           // definisco l'intenzione  
+           Object activity_cata_bastione_raid;
+		// passo all'attivazione dell'activity Pagina.java   
+           
+           
+         }
+		@Override
+	    public void onCreate (Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_cata_bastione_raid);
+
+}
+}
--- WoW-BossEncounter/src/com/example/wow_bossencounter/CataListaRaid.java
+++ WoW-BossEncounter/src/com/example/wow_bossencounter/CataListaRaid.java
@@ -1,29 +1,70 @@
 package com.example.wow_bossencounter;
 
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.app.Activity; 
+
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
-
-public class CataListaRaid extends Activity 
-implements OnClickListener {
-
+public class CataListaRaid extends Activity {
 	
-		
-		@Override    
-         public void onClick(View View) {    
-           // definisco l'intenzione  
-           Object activity_cata_list_raid;
-		// passo all'attivazione dell'activity Pagina.java   
-           
-           
-         }
+	ImageButton bastione;
+	ImageButton trono;
+	ImageButton anima;
+	ImageButton sotterranei;
+	ImageButton terre;
+	
+	
 		@Override
-	    public void onCreate (Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_cata_list_raid);
-		 
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cata_list_raid);
+        bastione = (ImageButton) this.findViewById(R.id.bdc);
+        trono = (ImageButton) this.findViewById(R.id.tdqv);
+        anima = (ImageButton) this.findViewById(R.id.add);
+        sotterranei = (ImageButton) this.findViewById(R.id.sdan);
+        terre = (ImageButton) this.findViewById(R.id.tdf);
 		}
-	}
+        	public void startbastioneboss (View ImageButton) {
+        		Intent intent = new Intent(this, BastioneListaBoss.class);
+        		startActivity(intent);
+        		}
+        	public void starttronoboss (View ImageButton) {
+        		Intent intent = new Intent(this, TronoListaBoss.class);
+        		startActivity(intent);
+        		}
+        	public void startanimaboss (View ImageButton) {
+        		Intent intent = new Intent(this, AnimaListaBoss.class);
+        		startActivity(intent);
+        		}
+        	public void startsotterraneiboss (View ImageButton) {
+        		Intent intent = new Intent(this, SotterraneiListaBoss.class);
+        		startActivity(intent);
+        	}
+        	public void startterreboss (View ImageButton) {
+            		Intent intent = new Intent(this, TerreListaBoss.class);
+            		startActivity(intent);
+        		}
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
