7afe0df54394937863ed55729b5d6c3496e4abe5
Nhova85 <valentino0185@gmail.com>
2014-11-04 23:43:19 +0100

    Test PDF su Halfus e Concilio

--- WoW-BossEncounter/src/com/example/wow_bossencounter/BastioneListaBoss.java
+++ WoW-BossEncounter/src/com/example/wow_bossencounter/BastioneListaBoss.java
@@ -5,14 +5,14 @@ import android.view.View.OnClickListener;
 import android.app.Activity; 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageButton;
+
 
 
 
 public class BastioneListaBoss extends Activity 
 implements OnClickListener {
 
+	
 	
 		
 		@Override    
@@ -27,6 +27,14 @@ implements OnClickListener {
 	    public void onCreate (Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_cata_bastione_raid);
+		}
 
+	        public void halfusguida (View Button) {
+        		Intent intent = new Intent(this, HalfusGuida.class);
+        		startActivity(intent);
+	        }
+            public void concilioguida (View Button) {
+            		Intent intent = new Intent(this, ConcilioGuida.class);
+            		startActivity(intent);
 }
 }
