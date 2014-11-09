a859e7104f81993d19310d42d76d3b3c17de139f
Nhova85 <valentino0185@gmail.com>
2014-11-09 12:18:27 +0100

    Prova PDF con Intent applicazione per PDF

--- WoW-BossEncounter/src/com/example/wow_bossencounter/BastioneListaBoss.java
+++ WoW-BossEncounter/src/com/example/wow_bossencounter/BastioneListaBoss.java
@@ -1,10 +1,20 @@
 package com.example.wow_bossencounter;
 
+import java.io.File;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfReader;
+
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.app.Activity; 
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+
+
 
 
 
@@ -12,7 +22,7 @@ import android.os.Bundle;
 public class BastioneListaBoss extends Activity 
 implements OnClickListener {
 
-	
+
 	
 		
 		@Override    
@@ -20,21 +30,26 @@ implements OnClickListener {
            // definisco l'intenzione  
            Object activity_cata_bastione_raid;
 		// passo all'attivazione dell'activity Pagina.java   
-           
-           
-         }
+		}
+         
+         
 		@Override
 	    public void onCreate (Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_cata_bastione_raid);
+	        setContentView(R.layout.activity_cata_bastione_raid);     
+	        
+	        Button button = (Button) findViewById(R.id.halfus);
+	        button.setOnClickListener(new View.OnClickListener() {
+	            @Override
+	            public void onClick(View arg0) {
+	            	File file = new File("/WoW-BossEncounter/res/raw/prova.PDF");
+	            	Uri path = Uri.fromFile(file);
+	            	Intent intent = new Intent(Intent.ACTION_VIEW);
+	            	intent.setDataAndType(path, "application/pdf");
+	            	startActivity(intent);
+	            	
+	    }
+	});
 		}
+}
 
-	        public void halfusguida (View Button) {
-        		Intent intent = new Intent(this, HalfusGuida.class);
-        		startActivity(intent);
-	        }
-            public void concilioguida (View Button) {
-            		Intent intent = new Intent(this, ConcilioGuida.class);
-            		startActivity(intent);
-}
-}
